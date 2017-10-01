a84801fdb90dcc7317c4e7935b9eaceb60a1ae11
jaridwade@atlanticbay.com <jaridwade@atlanticbay.com>
2017-09-30 22:17:08 -0400

    removing trailing whitespace

--- csvtest.js
+++ csvtest.js
@@ -2,6 +2,8 @@ const path = require('path');
 const Csv = require('./lib/csv');
 const csv = new Csv();
 
-csv.parseFile('./test/csvTest.csv').then(data => {
+csv.mapFile('./test/csvTest.csv').then(data => {
   console.log(data);
-});
+}).catch(e => {
+  console.log(e);
+})
